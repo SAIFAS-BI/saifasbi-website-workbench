b94c45ccb333a56b56164da6c804dd7434fdc979
dsanichsai <dsanich@saifas.org>
2021-12-29 10:31:52 +0300

    Head tag example improvements

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -5,3 +5,56 @@ window.onload = function () {
     document.body.classList.remove('loaded_hiding');
   }, 500);
 }
+
+Highcharts.setOptions({
+  colors: ['#01BAF2', '#71BF45', '#FAA74B', '#E83535']
+});
+Highcharts.chart('container', {
+    credits: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: 'Our experience'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false
+        },
+        showInLegend: true
+      }
+    },
+    series: [{
+      name: 'Composition',
+      colorByPoint: true,
+      data: [{
+        name: 'PowerBI',
+        y: 67,
+        sliced: true,
+        selected: true
+      }, {
+        name: 'Tableau',
+        y: 13
+      }, {
+        name: 'Google Data Studio',
+        y: 10
+      }, {
+        name: 'Qlik',
+        y: 10
+      }]
+    }]
+  });
