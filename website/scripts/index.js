ca1f7ff6728f40d9feb1bac6ecced8a5c562c6f5
dsanichsai <dsanich@saifas.org>
2021-12-29 14:28:59 +0300

    Circle chart improvements

--- website/scripts/index.js
+++ website/scripts/index.js
@@ -10,51 +10,103 @@ Highcharts.setOptions({
   colors: ['#01BAF2', '#71BF45', '#FAA74B', '#E83535']
 });
 Highcharts.chart('container', {
-    credits: {
-      enabled: false
-    },
-    exporting: {
-      enabled: false
-    },
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie'
-    },
-    title: {
-      text: 'Our experience'
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false
+  credits: {
+    enabled: false
+  },
+  exporting: {
+    enabled: false
+  },
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie',
+    height: 700,
+    width: 700,
+    responsive: {
+      rules: {
+        condition: {
+          maxHeight: 700,
+          maxWidth: 700,
+          minHeight: 200,
+          minWidth: 200
         },
-        showInLegend: true
+        chartOptions: {
+          legend: {
+            align: 'center',
+            verticalAlign: 'bottom',
+            layout: 'horizontal'
+          },
+          yAxis: {
+            labels: {
+              align: 'left',
+              x: 0,
+              y: -5
+            },
+            title: {
+              text: null
+            }
+          },
+          subtitle: {
+            text: null
+          },
+          credits: {
+            enabled: true
+          }
+        }
       }
-    },
-    series: [{
-      name: 'Composition',
-      colorByPoint: true,
-      data: [{
-        name: 'PowerBI',
-        y: 67,
-        sliced: true,
-        selected: true
-      }, {
-        name: 'Tableau',
-        y: 13
-      }, {
-        name: 'Google Data Studio',
-        y: 10
-      }, {
-        name: 'Qlik',
-        y: 10
-      }]
+    }
+  },
+  title: {
+    text: ''
+  },
+  legend: {
+    // align: 'right',
+    // verticalAlign: 'middle',
+    // layout: 'vertical',
+    // align: 'right',
+    // verticalAlign: 'middle',
+    // layout: 'vertical',
+    itemMarginBottom: 10,
+    itemStyle: {
+      fontSize: 16,
+      color: '#525252',
+      fontFamily: 'Open Sans',
+      fontWeight: 400
+    }
+  },
+  tooltip: {
+    pointFormat: ''
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: false,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: false
+      },
+      showInLegend: true
+    }
+  },
+  series: [{
+    name: 'Composition',
+    colorByPoint: true,
+    data: [{
+      name: 'PowerBI',
+      y: 67,
+      events: {
+        click: () => {document.location.href = "/powerbi/custom-visuals/"}
+      },
+      sliced: true
+    }, {
+      name: 'Tableau',
+      y: 13,
+    }, {
+      name: 'Google Data Studio',
+      y: 10
+    }, {
+      name: 'Qlik',
+      y: 10
     }]
-  });
+  }]
+});
