2ef443524237cb2bdcedcccfa9f6a927b37f4c35
dsanichsai <dsanich@saifas.org>
2022-01-06 13:16:53 +0300

    Added simple contact form (without styles)

--- website/assets/scripts/index.js
+++ website/assets/scripts/index.js
@@ -5,3 +5,17 @@ window.onload = function () {
     document.body.classList.remove('loaded_hiding');
   }, 500);
 }
+
+$(document).ready(function(){
+	$('.conctact-us-btn').on('click', function(e){
+		e.preventDefault();
+		$('#modal').fadeIn(100);
+		$('.modalBody').slideDown(300);
+
+	});
+	$('.modal-close, #modal').on('click', function(e){
+		e.preventDefault();
+		$('.modalBody').slideUp(100);
+		$('#modal').fadeOut(300);
+	});
+});
