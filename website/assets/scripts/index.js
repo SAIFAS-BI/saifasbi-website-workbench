3f410e0410deba6c48fd7c2eff2eddf1f3c43fdb
dsanich <dsanich@saifas.org>
2022-01-07 11:03:01 +0300

    Added home layout with anchors (without burger menu and responsive)

--- website/assets/scripts/index.js
+++ website/assets/scripts/index.js
@@ -19,3 +19,26 @@ $(document).ready(function(){
 		$('#modal').fadeOut(300);
 	});
 });
+
+///navbar
+
+$(window).scroll(function(){
+ var $sections = $('section');
+	$sections.each(function(i,el){
+    var top  = $(el).offset().top-100;
+    var bottom = top +$(el).height();
+    var scroll = $(window).scrollTop();
+    var id = $(el).attr('id');
+  	if( scroll > top && scroll < bottom){
+      $('a.active').removeClass('active');
+      $('a[href="#'+id+'"]').addClass('active');
+    }
+  })
+ });
+
+$("nav").on("click","a", function (event) {
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+  top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 800);
+});
